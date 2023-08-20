6c93e97570715c1d709c0cb50b7c38ebd29bd500
Godbell <12191579@inha.edu>
2023-08-21 03:56:03 +0900

    feat: integrate user router to db

--- routes/user.js
+++ routes/user.js
@@ -1,71 +1,102 @@
 import express from 'express';
 import { isFormatOf, isNullish, isValidEmail } from '../utils/validation.js';
+import dbConfig from '../utils/db_config.js';
+import { createConnection } from 'mariadb';
+import asyncify from 'express-asyncify';
 
-const userRouter = express.Router();
+const userRouter = asyncify(express.Router());
 
-userRouter.get('/', (req, res) => {
-  if (!isNullish(req.session.userIdx)) {
+userRouter.get('/', async (req, res) => {
+  const userIdx = req.session.userIdx;
+  if (isNullish(userIdx)) {
     res.sendStatus(401);
     return;
   }
 
-  const result = true;
-  console.log('get user');
+  const connection = await createConnection(dbConfig);
+  const query =
+    'SELECT id as idx, username, first_name, last_name, nickname, created_at, email' +
+    ' FROM user' +
+    ' WHERE id=?;';
+  const user = await connection.query(query, [userIdx]);
 
-  if (result === true) {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
+  connection.end();
+  res.json(user);
 });
 
-userRouter.get('/find-username', (req, res) => {
+userRouter.get('/find-username', async (req, res) => {
   const email = req.body.email;
   if (!isValidEmail(email)) {
     res.sendStatus(400);
     return;
   }
 
-  const result = true;
-  console.log('find username');
+  const connection = await createConnection(dbConfig);
+  const query = 'SELECT username FROM user WHERE email=?;';
+  const username = await connection.query(query, [email]);
 
-  if (result === true) {
-    res.sendStatus(200);
-  } else {
+  if (username.length === 0) {
     res.sendStatus(404);
+  } else {
+    res.json(username);
   }
+
+  connection.end();
 });
 
-userRouter.put('/reset-password', (req, res) => {
-  if (!isNullish(req.session.resetPwUserIdx)) {
-    res.sendStatus(401);
-    return;
-  }
-
-  const password = req.body.password;
-  if (!isFormatOf(password, { printables: true })) {
+userRouter.get('/reset-password/authenicate', async (req, res) => {
+  const email = req.query.email;
+  if (!isValidEmail(email)) {
     res.sendStatus(400);
     return;
   }
 
-  const result = true;
-  console.log('reset password');
+  const connection = await createConnection(dbConfig);
+  const query = 'SELECT id FROM user WHERE email=?';
+  const userIdx = await connection.query(query, [email]);
 
-  if (result === true) {
-    res.sendStatus(200);
-  } else {
+  if (userIdx.length === 0) {
     res.sendStatus(404);
+  } else {
+    req.session.pwResetUserIdx = userIdx[0];
   }
+
+  connection.end();
 });
 
-userRouter.put('/', (req, res) => {
-  if (!isNullish(req.session.userIdx)) {
+userRouter.put('/reset-password', async (req, res) => {
+  const userIdx = req.session.pwResetUserIdx;
+  if (isNullish(userIdx)) {
     res.sendStatus(401);
     return;
   }
 
   const password = req.body.password;
-  const isPasswordValid = isFormatOf(password, { printables: true });
+  if (!isFormatOf(password, { printables: true }) || password.length > 20) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const connection = await createConnection(dbConfig);
+  const query = 'UPDATE user SET password=? WHERE id=?';
+  await connection.query(query, [password, userIdx]);
+
+  connection.end();
+  res.sendStatus(200);
+});
+
+userRouter.put('/', async (req, res) => {
+  const userIdx = req.session.userIdx;
+  if (isNullish(userIdx)) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const password = req.body.password;
+  const isPasswordValid =
+    isFormatOf(password, { printables: true }) &&
+    password.length >= 8 &&
+    password.length <= 20;
 
   const nickname = req.body.nickname;
   const isNicknameValid =
@@ -73,18 +104,22 @@ userRouter.put('/', (req, res) => {
       alphabet: true,
       koreanComplete: true,
       number: true,
-    }) && nickname.length <= 45;
+    }) &&
+    nickname.length >= 2 &&
+    nickname.length <= 45;
 
   const firstName = req.body.firstName;
   const isFirstNameValid =
     isFormatOf(firstName, { alphabet: true, koreanComplete: true }) &&
     2 <= firstName.length &&
+    firstName.length >= 1 &&
     firstName.length <= 20;
 
   const lastName = req.body.lastName;
   const isLastNameValid =
     isFormatOf(lastName, { alphabet: true, koreanComplete: true }) &&
     2 <= lastName.length &&
+    lastName.length >= 1 &&
     lastName.length <= 20;
 
   const email = req.body.email;
@@ -101,18 +136,29 @@ userRouter.put('/', (req, res) => {
     return;
   }
 
-  const result = true;
-  console.log('edit user');
+  const connection = await createConnection(dbConfig);
 
-  if (result === true) {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
+  const query =
+    'UPDATE user SET nickname=?, firstname=?, lastname=?, email=?' +
+    (isNullish(password) ? '' : ' password=?') +
+    ' WHERE id=?';
+
+  const values = [];
+  values.push(nickname, firstName, lastName, email);
+  if (isNullish(password)) {
+    values.push(password);
   }
+  values.push(userIdx);
+
+  await connection.query(query, values);
+
+  connection.end();
+  res.sendStatus(200);
 });
 
 userRouter.delete('/', (req, res) => {
-  if (!isNullish(req.session.userIdx)) {
+  const authorIdx = req.session.userIdx;
+  if (isNullish(authorIdx)) {
     res.sendStatus(401);
     return;
   }
@@ -127,12 +173,21 @@ userRouter.delete('/', (req, res) => {
   }
 });
 
-userRouter.post('/', (req, res) => {
-  const result = true;
-  console.log('add user');
-
+userRouter.post('/', async (req, res) => {
   const password = req.body.password;
-  const isPasswordValid = isFormatOf(password, { printables: true });
+  const isPasswordValid =
+    isFormatOf(password, { printables: true }) &&
+    password.length >= 8 &&
+    password.length <= 20;
+
+  const username = req.body.username;
+  const isUsernameValid =
+    isFormatOf(username, {
+      alphabet: true,
+      number: true,
+    }) &&
+    username.length >= 8 &&
+    username.length <= 20;
 
   const nickname = req.body.nickname;
   const isNicknameValid =
@@ -140,25 +195,30 @@ userRouter.post('/', (req, res) => {
       alphabet: true,
       koreanComplete: true,
       number: true,
-    }) && nickname.length <= 45;
+    }) &&
+    nickname.length >= 2 &&
+    nickname.length <= 45;
 
   const firstName = req.body.firstName;
   const isFirstNameValid =
     isFormatOf(firstName, { alphabet: true, koreanComplete: true }) &&
     2 <= firstName.length &&
+    firstName.length >= 1 &&
     firstName.length <= 20;
 
   const lastName = req.body.lastName;
   const isLastNameValid =
     isFormatOf(lastName, { alphabet: true, koreanComplete: true }) &&
     2 <= lastName.length &&
+    lastName.length >= 1 &&
     lastName.length <= 20;
 
   const email = req.body.email;
   const isEmailValid = isValidEmail(email);
 
   if (
-    (!isNullish(password) && !isPasswordValid) ||
+    !isUsernameValid ||
+    !isPasswordValid ||
     !isNicknameValid ||
     !isFirstNameValid ||
     !isLastNameValid ||
@@ -168,27 +228,114 @@ userRouter.post('/', (req, res) => {
     return;
   }
 
-  if (result === true) {
-    res.sendStatus(200);
+  const connection = await createConnection(dbConfig);
+
+  if (emailCount[0].count !== 0) {
+    res.sendStatus(400);
+    connection.end();
+    return;
+  }
+
+  const updateQuery =
+    'INSERT INTO USER (nickname, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?);';
+
+  await connection.query(updateQuery, [
+    nickname,
+    firstName,
+    lastName,
+    email,
+    password,
+  ]);
+
+  connection.end();
+  res.sendStatus(200);
+});
+
+userRouter.get('/username-available', async (req, res) => {
+  const username = req.query.username;
+  const isUsernameValid =
+    isFormatOf(username, {
+      alphabet: true,
+      number: true,
+    }) &&
+    username.length >= 8 &&
+    username.length <= 20;
+
+  if (!isUsernameValid) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const connection = await createConnection(dbConfig);
+  const query = 'SELECT COUNT(*) AS count FROM user WHERE username=?';
+  const count = await connection.query(query, username);
+
+  connection.end();
+  if (count[0].count === 0) {
+    res.json({
+      availability: true,
+    });
   } else {
-    res.sendStatus(404);
+    res.json({
+      availability: false,
+    });
   }
 });
 
-userRouter.post('/signin', (req, res) => {
-  const result = true;
-  console.log('login');
+userRouter.get('/email-available', async (req, res) => {
+  const email = req.query.email;
+  const isEmailValid = isValidEmail(email);
 
-  if (result === true) {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
+  if (!isEmailValid) {
+    res.sendStatus(400);
+    return;
   }
+
+  const connection = await createConnection(dbConfig);
+  const query = 'SELECT COUNT(*) AS count FROM user WHERE email=?';
+  const count = await connection.query(query, email);
+
+  connection.end();
+  if (count[0].count === 0) {
+    res.json({
+      availability: true,
+    });
+  } else {
+    res.json({
+      availability: false,
+    });
+  }
+});
+
+userRouter.post('/signin', async (req, res) => {
+  const username = req.body.username;
+  const isUsernameValid = isFormatOf(username, {
+    alphabet: true,
+    number: true,
+  });
+
+  const password = req.body.password;
+  const isPasswordValid =
+    isFormatOf(password, { printables: true }) &&
+    password.length >= 8 &&
+    password.length <= 20;
+
+  if (!isUsernameValid || !isPasswordValid) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const connection = await createConnection(dbConfig);
+  const query = 'SELECT id AS idx FROM user WHERE username=? AND password=?;';
+  const userIdx = await connection.query(query, [username, password]);
+
+  req.session.userIdx = userIdx[0].idx;
 });
 
 userRouter.post('/signout', (req, res) => {
-  if (!isNullish(req.session.userIdx)) {
-    res.sendStatus(400);
+  const authorIdx = req.session.userIdx;
+  if (isNullish(authorIdx)) {
+    res.sendStatus(401);
     return;
   }
 
