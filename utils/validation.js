9e74688e1dd0f2775fb6b49123137b0172825256
Godbell <12191579@inha.edu>
2023-08-20 19:25:51 +0900

    feat: add email validator

--- utils/validation.js
+++ utils/validation.js
@@ -10,6 +10,16 @@ export const isLengthOf = (iterable, length) => {
   return iterable.length === length;
 };
 
+export const isValidEmail = (email) => {
+  if (isNullish(email) || isEmptyString(email)) return false;
+
+  const regex = new RegExp('[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$');
+  const name = email.split('@')[0] ?? '';
+  const domain = email.split('@')[1] ?? '';
+
+  return isLengthOf(name, 64) && isLengthOf(domain, 255) && regex.test(email);
+};
+
 export const isStringOfFormat = (
   str,
   { alphabet, korean, number, lineEndings },
