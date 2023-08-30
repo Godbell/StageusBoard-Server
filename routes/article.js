5b28ddf46edb1c15fa8105bc6d40037d50f98bda
Godbell <12191579@inha.edu>
2023-08-30 22:48:37 +0900

    stash 2308302248

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { isNullish, isNumber } from '../utils/validation.js';
+import { isNullish, isNumber, isValidUuid } from '../utils/validation.js';
 import asyncify from 'express-asyncify';
 import pgPool from '../utils/pgPool.js';
+import { v4 } from 'uuid';
 
 const articleRouter = asyncify(express.Router());
 
@@ -62,9 +63,10 @@ articleRouter.post('/', async (req, res) => {
   }
 
   const connection = await pgPool.connect();
-  const query =
+  const articleUploadQuery =
     'INSERT INTO backend.article (author_idx, title, content) VALUES ($1, $2, $3);';
-  await connection.query(query, [authorIdx, title, content]);
+  await connection.query(articleUploadQuery, [authorIdx, title, content]);
+
   connection.release();
 
   res.sendStatus(201);
@@ -132,17 +134,11 @@ articleRouter.get('/:idx/comment/all', async (req, res) => {
   }
 
   const connection = await pgPool.connect();
-  const query =
-    'SELECT backend.comment.idx, content, nickname,' +
-    " TO_CHAR(backend.article.created_at, 'YYYY-MM-DD HH24:MI:SS') AS created_at" +
-    ' FROM backend.comment' +
-    ' JOIN backend.user' +
-    ' ON author_idx=backend.user.idx' +
-    ' WHERE article_idx=?';
-  const comments = await connection.query(query, [articleIdx]);
+  const query = `SELECT data AS comments FROM backend.comment WHERE article_idx=$1;`;
+  const comments = (await connection.query(query, [articleIdx])).rows[0];
   connection.release();
 
-  res.json(comments);
+  res.json(comments ?? []);
 });
 
 articleRouter.post('/:idx/comment', async (req, res) => {
@@ -153,21 +149,33 @@ articleRouter.post('/:idx/comment', async (req, res) => {
   }
 
   const articleIdx = Number(req.params.idx);
-  if (!isNumber(articleIdx) || articleIdx < 0) {
-    res.sendStatus(400);
-    return;
-  }
-
   const content = req.body.content;
-  if (isNullish(content)) {
+  const ref = req.body.ref ?? null;
+  const isInputValid =
+    isNumber(articleIdx) &&
+    articleIdx >= 0 &&
+    isNumber(depth) &&
+    depth >= 0 &&
+    !isNullish(content) &&
+    (ref === null || isValidUuid(ref));
+  if (isInputValid) {
     res.sendStatus(400);
     return;
   }
 
   const connection = await pgPool.connect();
-  const query =
-    'INSERT INTO backend.comment (article_idx, author_idx, content) VALUES ($1, $2, $3);';
-  await connection.query(query, [articleIdx, authorIdx, content]);
+
+  const currentCommentsQuery = `SELECT data FROM backend.comment WHERE article_idx=$1;`;
+  const comments = (await connection.query(currentCommentsQuery, [articleIdx]))
+    .rows[0] ?? {
+    comments: [],
+  };
+
+  if (!JSON.stringify(comments).includes(`"id": "${ref}"`)) {
+    res.sendStatus(400);
+    return;
+  }
+
   connection.release();
 
   res.sendStatus(201);
