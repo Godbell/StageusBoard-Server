e6b67e826db0bd1903aeb31bc98161c7eb6e057b
Godbell <12191579@inha.edu>
2023-09-04 13:22:37 +0900

    refactor: change rdb env var name to RDB_...

--- utils/pgPool.js
+++ utils/pgPool.js
@@ -5,11 +5,11 @@ const { Pool } = pg;
 configDotenv();
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.RDB_HOST,
+  port: process.env.RDB_PORT,
+  user: process.env.RDB_USER,
+  password: process.env.RDB_PASSWORD,
+  database: process.env.RDB_NAME,
 };
 
 const pgPool = new Pool(dbConfig);
