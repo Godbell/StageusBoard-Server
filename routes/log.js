0db40134f99a886491a8975c95149990e7bd4593
Godbell <12191579@inha.edu>
2023-09-11 17:10:25 +0900

    feat: enable ip, method, url filter to log getter

--- routes/log.js
+++ routes/log.js
@@ -2,26 +2,33 @@ import { Router } from 'express';
 import asyncify from 'express-asyncify';
 import mongoPool from '../utils/mongoPool.js';
 import { logSchema } from '../models/log.js';
+import escapeStringRegexp from 'escape-string-regexp';
 
 export const logRouter = asyncify(Router());
 
 logRouter.get('/', async (req, res) => {
   const startDate = new Date(req.query.startDate ?? 0);
   const endDate = new Date(req.query.endDate ?? 0);
+  const { ip, method, url } = req.query;
 
-  const result = (
-    await mongoPool
-      .model('log', logSchema)
-      .find({
-        created_at: {
-          $gte: startDate,
-          $lte: endDate,
-        },
-      })
-      .exec()
-  ).map((log) => {
-    log.ip, log.method, log.url, log.created_at;
-  });
+  const result = await mongoPool
+    .model('log', logSchema)
+    .find({
+      created_at: {
+        $gte: startDate,
+        $lte: endDate,
+      },
+      ip: {
+        $regex: ip ? `(.+)?${escapeStringRegexp(ip)}(.+)?` : '',
+      },
+      method: {
+        $regex: method ? `(.+)?${escapeStringRegexp(method)}(.+)?` : '',
+      },
+      url: {
+        $regex: url ? `(.+)?${escapeStringRegexp(url)}(.+)?` : '',
+      },
+    })
+    .exec();
 
   res.json(result);
 });
